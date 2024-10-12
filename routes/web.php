1fd7828a824595e2814e8955c9cf28947878fb61
Пахомов Евгений <nightmare23@yandex.ru>
2024-10-12 14:48:01 +0700

    Add HW3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FormProcessor;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,4 +12,6 @@ Route::match(['get', 'post'], '/userform', [FormProcessor::class, 'index']);
 
 Route::post('/store_form', [FormProcessor::class, 'store']);
 
-Route::get('/hello/{firstname}', [FormProcessor::class, 'hello'])->name('hello'); 
+Route::get('/hello/{firstname}', [FormProcessor::class, 'hello'])->name('hello');
+
+Route::get('/test_database', EmployeeController::class);
