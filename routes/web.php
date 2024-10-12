a63fcd626cda97f72a221399e878db31be089b42
Пахомов Евгений <nightmare23@yandex.ru>
2024-10-12 11:17:48 +0700

    Homework1,2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\FormProcessor;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::match(['get', 'post'], '/userform', [FormProcessor::class, 'index']);
+
+Route::post('/store_form', [FormProcessor::class, 'store']);
+
+Route::get('/hello/{firstname}', [FormProcessor::class, 'hello'])->name('hello'); 
